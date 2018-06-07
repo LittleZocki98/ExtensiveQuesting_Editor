702845eb6cd60b93da9ed72e22570d5fba4028a2
LittleZocki98 <littlezocki98@gmail.com>
2018-06-07 23:55:49 +0200

    Organised and cleaned usings in Criteria, Tasks and Rewards

--- ExtensiveQuesting/ExtensiveQuesting/BaseReward.cs
+++ ExtensiveQuesting/ExtensiveQuesting/BaseReward.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ExtensiveQuesting.EnumDescription;
-using ExtensiveQuesting.QuestingItem;
+﻿using ExtensiveQuesting.EnumDescription;
 using ExtensiveQuesting.QuestingItem.Quest;
 using Newtonsoft.Json;
 
--- ExtensiveQuesting/ExtensiveQuesting/TaskCollect.cs
+++ ExtensiveQuesting/ExtensiveQuesting/TaskCollect.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ExtensiveQuesting.QuestingItem.Item;
+﻿using ExtensiveQuesting.QuestingItem.Item;
 using Newtonsoft.Json;
 
 namespace ExtensiveQuesting.QuestingItem.Task {
